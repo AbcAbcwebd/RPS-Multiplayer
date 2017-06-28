8f26a4960566ab9c141e6ff13c7e4f438f4d18af
Owen Essen <owenessen@Owens-Air.PK5001Z>
2017-06-27 22:45:51 -0400

    Firebase stuff becoming functional.

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+// When a user first comes to the page, they have not yet set their username.
+var userSet = false;
+
+// Prompts the user to set their username.
+// Dynamically generated so that it can more easily be removed and readed to the page as necesary. 
+function getUserName(){
+	$('#account-display').empty();
+	var instructions = $('<p>').text("Please pick a screen name to start playing.");
+	var nameInput = $('<input>').attr("type", "text");
+	nameInput.attr("id", "name-input");
+	var submitButton = $('<button>').text("Set Name");
+	submitButton.attr("id", "set-name-button");
+	$('#account-display').append(instructions);
+	$('#account-display').append(nameInput);
+	$('#account-display').append(submitButton);
+}
+
+// Click functionality goes here
+$( document ).ready(function() {
+	$("#set-name-button").on("click", function() {
+		event.preventDefault();
+		var userInput = $('#name-input').val();
+		console.log(userInput);
+	});
+});
+
